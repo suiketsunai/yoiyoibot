a95eaced2174b1d6be4cc908d6ff570c9b8bd04d
suiketsunai <nvrmndr.desu@yandex.ru>
2022-03-27 04:33:58 +0300

    Add database

---
+++ db/__init__.py
@@ -0,0 +1,18 @@
+import os
+
+# working with env
+from dotenv import load_dotenv
+
+# create engine
+from sqlalchemy import create_engine
+
+from db.models import Base
+
+# load .env file & get config
+load_dotenv()
+
+engine = create_engine(
+    os.environ["DATABASE_URI"].replace("postgres://", "postgresql://"),
+    echo=True,
+    future=True,
+)
---
+++ db/models.py
@@ -0,0 +1,33 @@
+"""Database module"""
+from sqlalchemy import (
+    Column,
+    BigInteger,
+    String,
+    Boolean,
+)
+from sqlalchemy.orm import declarative_base
+
+# pretty __repr__ and __str__
+from sqlalchemy_repr import RepresentableBase
+
+Base = declarative_base(cls=RepresentableBase)
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    # telegram id
+    id = Column(BigInteger, primary_key=True, autoincrement=False)
+    # telegram full name
+    full_name = Column(String)
+    # telegram user name
+    nick_name = Column(String)
+    # last sent link (currently not used)
+    last_link = Column(String)
+
+    # twitter original mode
+    tw_orig = Column(Boolean, default=False, nullable=False)
+    # tiktok hd mode
+    tt_orig = Column(Boolean, default=False, nullable=False)
+    # instagram original mode
+    in_orig = Column(Boolean, default=False, nullable=False)
